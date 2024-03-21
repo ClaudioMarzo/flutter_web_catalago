67014fa96a3750e6aed5ff33cc126b33ddc73e9d
Claudio Santarem <claudiosantarem@bemol.com.br>
2024-03-20 23:22:03 -0400

    Adicionando imagem na app bar

--- lib/app/features/home/home_page.dart
+++ lib/app/features/home/home_page.dart
@@ -11,8 +11,20 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Katy Festa'),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(150.0), // Define a altura da AppBar
+        child: AppBar(
+          title: Center(
+            child: Image.asset(
+              'assets/images/logo.png', // Caminho da imagem
+              fit: BoxFit.cover, // Ajusta a imagem para cobrir a área disponível
+              height: 60.0, // Define a altura da imagem para corresponder à altura da AppBar
+            ),
+          ),
+          centerTitle: true, // Centraliza o título
+          backgroundColor: Colors.transparent, // Torna o fundo da AppBar transparente
+          elevation: 0, // Remove a sombra da AppBar
+        ),
       ),
       body: Container(),
     );
