900c254bb4e2013f6a94af705e575d3d749683d5
Claudio Santarem <claudiosantarem@bemol.com.br>
2024-03-20 21:02:21 -0400

    Configuracao inicial do FireBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:katyfestacatalago/firebase_options.dart';
 import 'package:katyfestacatalago/app/features/app_widget.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AppWidget());
 }
