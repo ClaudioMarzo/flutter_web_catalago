cbf6102cf384ff6ee14631a25ffcaeeff3fbc650
Claudio Santarem <claudiosantarem@bemol.com.br>
2024-03-21 23:56:59 -0400

    Adicionando mais cores

--- lib/app/core/ui/style/color_style.dart
+++ lib/app/core/ui/style/color_style.dart
@@ -10,11 +10,13 @@ class ColorStyle {
     return _instance!;
   }
 
-  Color get white => const Color.fromRGBO(255, 255, 255, 1);
-  Color get darkwhite => const Color.fromRGBO(240, 240, 245, 1);
+  Color get black => const Color.fromRGBO(0, 41, 59, 1);
   Color get blue => const Color.fromRGBO(63, 142, 189, 1);
   Color get gray => const Color.fromRGBO(104, 112, 118, 1);
-  Color get black => const Color.fromRGBO(0, 41, 59, 1);
+  Color get pink => const Color.fromRGBO(242, 134, 148, 1);
+  Color get white => const Color.fromRGBO(255, 255, 255, 1);
+  Color get darkwhite => const Color.fromRGBO(240, 240, 245, 1);
+  Color get lightblue => const Color.fromRGBO(143, 171, 188, 1);
 }
 
 extension ColorsAppExtension on BuildContext {
