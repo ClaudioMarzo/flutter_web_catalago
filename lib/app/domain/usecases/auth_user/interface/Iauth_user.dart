480ec4e0d9e39e5da9f4e03b344f8656a9d6ca91
Claudio Santarem <claudiosantarem@bemol.com.br>
2024-03-28 14:45:12 -0400

    Removendo pasta desnecessaria

--- lib/app/domain/usecases/auth_user/interface/Iauth_user.dart
+++
@@ -1,5 +0,0 @@
-import 'package:katyfestacatalago/app/domain/entities/user_entity.dart';
-
-abstract class IAuthUser {
-  Future<Map<String, dynamic>> signInWithEmailAndPassword(UserEntity credentials);
-}
