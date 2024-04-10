f8e91f60abf6c15cfad5439f419497b64bd72106
Claudio Santarem <claudiosantarem@bemol.com.br>
2024-04-09 22:31:21 -0400

    adicionado configurancao do flutter modular

--- lib/app/presentation/app_widget.dart
+++ lib/app/presentation/app_widget.dart
@@ -1,27 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:katyfestacatalago/app/presentation/screen/home/home_page.dart';
-import 'package:katyfestacatalago/app/presentation/screen/home/widgets/appbar/auth/auth_dialog_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:katyfestacatalago/app/presentation/widgets/theme/theme_config.dart';
 
-class AppWidget extends StatefulWidget {
+class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
-  State<AppWidget> createState() => _AppWidgetState();
-}
-
-class _AppWidgetState extends State<AppWidget> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'KatyFestaCatalago',
       theme: ThemeConfig.theme,
-      initialRoute: '/',
-      routes: {
-        '/': (_) => const HomePage(),
-        '/auth': (_) => const AuthDialogPage(),
-      },
+      routerConfig: Modular.routerConfig,
     );
   }
 }
--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:katyfestacatalago/app/presentation/bloc/auth/auth_bloc.dart';
 import 'package:katyfestacatalago/app/presentation/screen/home/home_page.dart';
+import 'package:katyfestacatalago/app/infrastructure/datasources/interfaces/Iauth_user_datasource.dart';
+import 'package:katyfestacatalago/app/infrastructure/datasources/firebase/auth_user_datasource_firebase.dart';
 
 class AppModule extends Module {
   @override
-  void binds(i) {}
+  void binds(i) {
+    // i.add(GoogleAuthProvider.new);
+    // i.addInstance(FirebaseAuth.instance);
+    // i.add<IAuthUserDataSource>(AuthUserDatasourceFirebase.new);
+    // i.addSingleton(AuthBloc.new);
+  }
 
   @override
   void routes(r) {
