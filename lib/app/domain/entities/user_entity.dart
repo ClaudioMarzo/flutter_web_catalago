4031b8d3ac3fb23ef2ac63fff732a7a72ecea39a
Claudio Santarem <claudiosantarem@bemol.com.br>
2024-04-09 22:20:40 -0400

    adicionando user cases

--- lib/app/domain/entities/user_entity.dart
+++ lib/app/domain/entities/user_entity.dart
@@ -6,4 +6,16 @@ class UserEntity {
     required this.email,
     required this.senha,
   });
+
+  UserEntity copyWith({String? email, String? senha}) {
+    return UserEntity(
+      email: _validateEmail(email!) ? email : throw ('Email inválido'),
+      senha: senha ?? this.senha,
+    );
+  }
+
+  static bool _validateEmail(String email) {
+    final RegExp emailRegex = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+    return emailRegex.hasMatch(email);
+  }
 }
